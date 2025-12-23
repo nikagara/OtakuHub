84465ba7a84c7d026b3e328f4d786daf0530f809
Velios <timuss483@gmail.com>
2025-12-23 11:46:36 +0300

    awsd

--- php/src/registration.php
+++ php/src/registration.php
@@ -1,9 +1,10 @@
 <?php
 
-require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/helpers.php';  
+
 
 // получение данных из формы регистрации
-
+// $sql = "SELECT FROM `users`";
 $login = $_POST['login'];
 $password = $_POST['password'];
 
@@ -11,10 +12,11 @@ $password = $_POST['password'];
 
 $connect = getDB();
 
-$sql = "INSERT INTO `users` (login, pfssword) VALUES ('$login', '$password')";
+$sql = "INSERT INTO `users` (login, password) VALUES ('$login', '$password')";
 
 if ($connect -> query($sql) === TRUE) {
-     echo 'Регистрация прошла успешно!';
+    //  echo 'Регистрация прошла успешно!';
+    header("Location: /login.html");
 } else {
     echo 'Данный пользователь уже зарегистрирован :(';
 }

4ac29db82bee14c71cd6e4fd4ca47075a4f38541
Velios <timuss483@gmail.com>
2025-12-23 19:26:32 +0300

    1

--- index.php
+++ index.php
@@ -14,12 +14,13 @@
     <input type="text" placeholder="password" name="pass">
     <input type="text" placeholder="repeat password" name="repeatpass">
     <input type="text" placeholder="email" name="email">
+    <button type="submit">Зарегистрироваться</button>
 </form>
 
-<form action="register.php" method="post">
+<form action="login.php" method="post">
     <input type="text" placeholder="login" name="login">
     <input type="text" placeholder="password" name="pass">
-
+    <button type="submit">Войти</button>
 </form>
 
 
---
+++ register.php
@@ -0,0 +1,6 @@
+<?php
+require_once('db.php');
+$login = $_POST['login'];
+$pass = $_POST['pass'];
+$repeatpass = $_POST['repeatpass'];
+$email = $_POST['email'];
